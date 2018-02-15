64544392d49102311f960fc56834920771d9c1b1
JohansonSerrano <36167828+JohansonSerrano@users.noreply.github.com>
2018-02-15 13:27:06 -0500

    Add files via upload

---
+++ 06/LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+	public static void main( String[] args )
+	{
+		System.out.println ( "+---------------------------------------------------------+" );
+		System.out.println ( "|                                                    #### |" );
+		System.out.println ( "|                                                    #### |" );
+		System.out.println ( "|                                                    #### |" );
+		System.out.println ( "|                                                         |" );
+		System.out.println ( "|                                                         |" );
+		System.out.println ( "|                              Johanson Serrano           |" );
+		System.out.println ( "|                              8 Ludford Drive            |" );
+		System.out.println ( "|                              Richmond Hill, ON L4B4H6   |" );
+		System.out.println ( "|                                                         |" );
+		System.out.println ( "+---------------------------------------------------------+" );
+	}
+}
---
+++ 07/YourInitials.java
@@ -0,0 +1,15 @@
+public class YourInitials
+{
+	public static void main( String[] args )
+	{
+		System.out.println ("For the name Johanson Huang Serrano...");
+		System.out.println (" ");
+		System.out.println ("JJJJJ  H   H   SSS");
+		System.out.println ("  J    H   H  S   S");
+		System.out.println ("  J    H   H  S");
+		System.out.println ("  J    HHHHH   SSS");
+		System.out.println ("J J    H   H      S");
+		System.out.println ("J J    H   H  S   S");
+		System.out.println (" JJ    H   H   SSS");
+	}
+}	
