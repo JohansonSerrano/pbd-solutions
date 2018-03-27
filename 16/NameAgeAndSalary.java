a507e4a474759ae78a7017f8d2bccd2c761d5fb1
JohansonSerrano <36167828+JohansonSerrano@users.noreply.github.com>
2018-03-27 09:36:41 -0400

    Add files via upload

--- 16/NameAgeAndSalary.java
+++ 16/NameAgeAndSalary.java
@@ -4,12 +4,12 @@ public class NameAgeAndSalary {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 		
-		System.out.print("Hello.  What is your name?\n");
+		System.out.println("Hello.  What is your name?");
         String name = input.next();
-		System.out.print("Hi, " + name + "!  How old are you?\n");
+		System.out.println("Hi, " + name + "!  How old are you?");
 		int age = input.nextInt();
-		System.out.print("So you're " + age + ", eh?  That's not old at all!" + "\nHow much do you make, " + name + "?\n");
+		System.out.println("So you're " + age + ", eh?  That's not old at all!" + "\nHow much do you make, " + name + "?");
         double salary = input.nextDouble();
-		System.out.print(salary + "!  I hope that's per hour and not per year! LOL!");
+		System.out.println(salary + "!  I hope that's per hour and not per year! LOL!");
 	}
 }
